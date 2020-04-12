37c468024955f38d23eccae568f4b8a162633f14
Miguel Rochefort <miguelrochefort@gmail.com>
2020-04-12 01:02:22 -0400

    Add naive repetition counting algorithm

--- android/app/src/main/java/org/tensorflow/lite/examples/posenet/RepetitionCounter.kt
+++ android/app/src/main/java/org/tensorflow/lite/examples/posenet/RepetitionCounter.kt
@@ -5,15 +5,62 @@ import org.tensorflow.lite.examples.posenet.lib.Person
 import java.util.*
 
 class RepetitionCounter {
+    val MIN_AMPLITUDE = 10
+    val REP_THRESHOLD = 0.8
+    val MIN_CONFIDENCE = 0.5
 
+    var count = 0
+
+    var first = true
+    var goal = 1
+    var prev_y = 0
+    var prev_dy = 0
+    var top = 0
+    var bottom = 0
+
+    fun OnFrame(person: Person) : Int {
+        //WriteToCsv(person)
+
+        // Simple repetition count using only the nose as key point
+        if (person.keyPoints[BodyPart.NOSE.ordinal].score >= MIN_CONFIDENCE) {
+            var y = 10000 - person.keyPoints[BodyPart.NOSE.ordinal].position.y
+            var dy = y - prev_y
+            if (!first) {
+                if (bottom != 1000000 && top != 0) {
+                    if (goal == 1 && dy > 0 && (y - bottom) > (top - bottom) * REP_THRESHOLD) {
+                        if (top - bottom > MIN_AMPLITUDE) {
+                            count++
+                            goal = -1
+                        }
+                    }
+                    else if (goal == -1 && dy < 0 && (top - y) > (top - bottom) * REP_THRESHOLD) {
+                        goal = 1
+                    }
+                }
+
+                // TODO: Use MIN_AMPLITUDE
+                if (dy < 0 && prev_dy >= 0 && prev_y - bottom > MIN_AMPLITUDE) {
+                    top = prev_y
+                }
+                else if (dy > 0 && prev_dy <= 0 && top - prev_y > MIN_AMPLITUDE) {
+                    bottom = prev_y
+                }
+            }
+
+            first = false
+            prev_y = y
+            prev_dy = dy
+        }
+
+        return count
+    }
+
+    // Write frames to CSV for later analysis in Google Colab
     var csv = ""
-
-    fun OnFrame(person: Person) {
-        // Write frames to CSV for later analysis in Google Colab
-        val now = System.currentTimeMillis()
-        val y = person.keyPoints.map { kp -> if (kp.score >= 0.5) kp.position.y.toString() else "" }
-        val values = y.joinToString(",")
-
-        csv += "${now},${values}\n"
+    fun WriteToCsv(person: Person) {
+        val x = System.currentTimeMillis()
+        val ys = person.keyPoints.map { kp -> if (kp.score >= 0.5) kp.position.y.toString() else "" }
+        val values = ys.joinToString(",")
+        csv += "${x},${values}\n"
     }
 }
